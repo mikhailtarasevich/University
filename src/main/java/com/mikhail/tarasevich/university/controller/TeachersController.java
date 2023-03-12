3a32c962099af6a3910d176779467d3108bcd81e
mikhailtarasevich <tarasevichmikhail93@gmail.com>
2023-03-12 16:04:02 +0400

    Added empty  TeachersController.class.

---
+++ src/main/java/com/mikhail/tarasevich/university/controller/TeachersController.java
@@ -0,0 +1,9 @@
+package com.mikhail.tarasevich.university.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("teachers")
+public class TeachersController {
+}
