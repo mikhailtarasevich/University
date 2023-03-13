b5a4fc35ed8ff9f254a001b84f1607fd71ee0f60
mikhailtarasevich <tarasevichmikhail93@gmail.com>
2023-03-13 11:06:47 +0400

    Added methods showAllTeachers and showTeacher to controller and views for them.

--- src/main/java/com/mikhail/tarasevich/university/controller/TeachersController.java
+++ src/main/java/com/mikhail/tarasevich/university/controller/TeachersController.java
@@ -1,9 +1,39 @@
 package com.mikhail.tarasevich.university.controller;
 
+import com.mikhail.tarasevich.university.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("teachers")
 public class TeachersController {
+
+    private final TeacherService teacherService;
+
+    @Autowired
+    public TeachersController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @GetMapping
+    public String showAllTeachers (@RequestParam(value = "page", defaultValue = "1") int page, Model model) {
+        model.addAttribute("teachers", teacherService.findAll(String.valueOf(page)));
+        model.addAttribute("lastPage", teacherService.lastPageNumber());
+        model.addAttribute("currentPage", page);
+
+        return "teachers/all-teachers";
+    }
+
+    @GetMapping("/{id}")
+    public String showTeacher (@PathVariable(value = "id") int id, Model model) {
+        model.addAttribute("teacher", teacherService.findById(id));
+
+        return "teachers/show-teacher";
+    }
+
 }
