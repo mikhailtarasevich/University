bf04deeee325a59e43fc8d98840497fddfe44049
mikhailtarasevich <tarasevichmikhail93@gmail.com>
2023-04-11 17:51:08 +0400

    Added Jacoco for maven multi-module application, Dockerfile, and docker-compose.yml, in which the tomcat-server and postgres containers work together with bridge and JNDI for DataSource initialization.

--- university-data/src/main/java/com/mikhail/tarasevich/university/config/PersistenceConfig.java
+++ university-data/src/main/java/com/mikhail/tarasevich/university/config/PersistenceConfig.java
@@ -5,35 +5,27 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.util.ResourceBundle;
 
 @Configuration
 @ComponentScan("com.mikhail.tarasevich.university")
 @EnableTransactionManagement
 public class PersistenceConfig {
 
-    private static final String DB_PROPERTIES_FILE_PATH = "database";
-
     @Bean
-    public PlatformTransactionManager transactionManager() {
+    public PlatformTransactionManager transactionManager() throws NamingException {
         return new DataSourceTransactionManager(dataSource());
     }
 
     @Bean
-    public DataSource dataSource(){
-        ResourceBundle resource = ResourceBundle.getBundle(DB_PROPERTIES_FILE_PATH);
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(resource.getString("db.driverClass"));
-        dataSource.setUrl(resource.getString("db.url"));
-        dataSource.setUsername(resource.getString("db.user"));
-        dataSource.setPassword(resource.getString("db.password"));
-
-        return dataSource;
+    public DataSource dataSource() throws NamingException {
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        return (DataSource) jndiTemplate.lookup("java:comp/env/jdbc/db");
     }
 
     @Bean
